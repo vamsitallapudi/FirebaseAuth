1ecf68010908b239fc9ddcd83d4710ecb0b903c5
Vamsi Tallapudi <vtallapudi@processmap.com>
2018-07-17 15:39:07 +0530

    fine-tune success animation

--- app/src/main/java/com/coderefer/firebaseauth/AuthActivity.kt
+++ app/src/main/java/com/coderefer/firebaseauth/AuthActivity.kt
@@ -9,6 +9,7 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.os.Build
 import android.os.Handler
+import android.view.View.GONE
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import com.airbnb.lottie.LottieAnimationView
@@ -24,12 +25,11 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
+        successFailAnimation = findViewById(R.id.logging_animation)
         cardView = findViewById(R.id.cv_login)
         cardView.setOnClickListener {
             tvLogin = findViewById(R.id.tv_login)
             tvLogin.visibility = View.INVISIBLE
-            successFailAnimation = findViewById(R.id.logging_animation)
             addSuccessFailAnimationListener()
             scaleView(cardView)
         }
@@ -47,19 +47,23 @@ class AuthActivity : AppCompatActivity() {
 
             override fun onAnimationEnd(p0: Animator?) {
 
+                val startSuccessFrame = 300
+                val endSuccessFrame = 390
+
                 if (success) {
                     successFailAnimation.cancelAnimation()
-                    successFailAnimation.setMinAndMaxFrame(200, 400)
+                    successFailAnimation.setMinAndMaxFrame(startSuccessFrame, endSuccessFrame)
                     successFailAnimation.playAnimation()
+                    successFailAnimation.repeatCount = 0 // to make sure animation doesn't repeat
                 } else {
-                    successFailAnimation.setMinAndMaxFrame(600, 800)
+                    successFailAnimation.setMinAndMaxFrame(700, 800)
                 }
-                if (successFailAnimation.frame ==200) {
-                    handler.postDelayed({
-                        successFailAnimation.visibility = View.GONE
+                successFailAnimation.addAnimatorUpdateListener {
+                    if (successFailAnimation.frame == 375) {
+                        successFailAnimation.visibility = GONE
                         revealAnimation()
-                    },3500)
-                    success = false
+                        success = false
+                    }
                 }
             }
 
@@ -71,10 +75,10 @@ class AuthActivity : AppCompatActivity() {
     }
 
     private fun revealAnimation() {
-
         /**
          *  to create circular reveal animation for next screen transition
          * */
+
         bgView = findViewById(R.id.revealView)
 
         // Check if the runtime version is at least Lollipop
@@ -108,39 +112,44 @@ class AuthActivity : AppCompatActivity() {
         }
     }
 
-    fun scaleView(v: View) {
+    private fun scaleView(v: View) {
+        /**
+         * to scale down the view (here, login button) to zero size upon clicking
+        */
+        successFailAnimation.visibility=GONE
         val anim = ScaleAnimation(
                 1f, 0f, // Start and end values for the X axis scaling
                 1f, 1f, // Start and end values for the Y axis scaling
                 Animation.RELATIVE_TO_SELF, .5f, // Pivot point of X scaling
                 Animation.RELATIVE_TO_SELF, .5f) // Pivot point of Y scaling
         anim.fillAfter = true // Needed to keep the result of the animation
-        anim.duration = 200
-        val animListener = AnimationListenerer()
+        anim.duration = 300
+        val animListener = AnimationListener()
         anim.setAnimationListener(animListener)
         v.startAnimation(anim)
     }
 
     private val handler: Handler
         get() {
-            val handler = Handler()
-            return handler
+            return Handler()
         }
 
-    inner class AnimationListenerer: Animation.AnimationListener{
+    inner class AnimationListener: Animation.AnimationListener{
+        /**
+         * Animation listener created for Login Button
+         * */
         override fun onAnimationRepeat(p0: Animation?) {
 
         }
 
         override fun onAnimationEnd(p0: Animation?) {
-            successFailAnimation.setMinAndMaxFrame(0, 200)
+            successFailAnimation.setMinAndMaxFrame(0, 100)
             successFailAnimation.playAnimation()
             successFailAnimation.visibility = View.VISIBLE
 
         }
 
         override fun onAnimationStart(p0: Animation?) {
-            successFailAnimation.visibility = View.VISIBLE
         }
     }
 }
